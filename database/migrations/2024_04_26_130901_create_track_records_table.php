6585b458506c1ccfe4ef9a82d13c5562e079aeb6
CandhyFadhila <omaplocandhy@gmail.com>
2024-04-26 21:19:06 +0700

    Init

---
+++ database/migrations/2024_04_26_130901_create_track_records_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('track_records', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->date('tgl_masuk');
+            $table->date('tgl_keluar')->nullable();
+            $table->string('promosi')->nullable();
+            $table->string('mutasi')->nullable();
+            $table->string('reward')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('track_records');
+    }
+};
