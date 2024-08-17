806c602ffe5aca4e8c56b6b724225fdaee2e04dc
CandhyFadhila <omaplocandhy@gmail.com>
2024-08-17 12:15:13 +0700

    UPDATE: Message array on PTKP

--- app/Http/Requests/StorePTKPRequest.php
+++ app/Http/Requests/StorePTKPRequest.php
@@ -48,9 +48,11 @@ class StorePTKPRequest extends FormRequest
 
     public function failedValidation(Validator $validator)
     {
+        $messages = implode(' ', $validator->errors()->all());
         $reponse = [
             'status' => Response::HTTP_BAD_REQUEST,
-            'message' => $validator->errors()
+            // 'message' => $validator->errors()
+            'message' => $messages
         ];
 
         throw new HttpResponseException(response()->json($reponse, Response::HTTP_BAD_REQUEST));
