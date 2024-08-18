2d0e1f1be5b4c0113f42be10cd7c530e4b95217b
CandhyFadhila <omaplocandhy@gmail.com>
2024-08-18 11:30:51 +0700

    UPDATE: New shift request

---
+++ app/Http/Requests/StoreShiftRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Http\Response;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreShiftRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nama' => 'required|string|max:225',
+            'jam_from' => 'required|string',
+            'jam_to' => 'required|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nama.required' => 'Nama shift tidak diperbolehkan kosong.',
+            'nama.string' => 'Nama shift tidak diperbolehkan mengandung angka.',
+            'nama.max' => 'Nama shift melebihi batas maksimum panjang karakter.',
+            'jam_from.required' => 'Jam kerja mulai shift tidak diperbolehkan kosong.',
+            'jam_from.string' => 'Nama shift tidak diperbolehkan mengandung selain angka dan huruf.',
+            'jam_to.required' => 'Jam kerja selesai shift tidak diperbolehkan kosong.',
+            'jam_to.string' => 'Nama shift tidak diperbolehkan mengandung selain angka dan huruf.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        $reponse = [
+            'status' => Response::HTTP_BAD_REQUEST,
+            'message' => $validator->errors()
+        ];
+
+        throw new HttpResponseException(response()->json($reponse, Response::HTTP_BAD_REQUEST));
+    }
+}
