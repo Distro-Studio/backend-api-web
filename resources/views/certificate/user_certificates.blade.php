e35934bdbc53a7ac8ab202c570aecfe7f2ec6dc1
CandhyFadhila <candhy.fadhila.arsyad@gmail.com>
2025-12-07 06:18:17 +0700

    FIXED: Refactor the internal training certificate template

--- resources/views/certificate/user_certificates.blade.php
+++ resources/views/certificate/user_certificates.blade.php
@@ -1,110 +1,121 @@
 <!DOCTYPE html>
 <html lang="id">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Sertifikat</title>
+        <style>
+            @page {
+                size: A4 landscape;
+                margin: 0;
+            }
 
-<head>
-	<meta charset="UTF-8">
-	<title>Sertifikat</title>
-	<style>
-		body {
-			font-family: "Times New Roman", serif;
-			padding: 60px;
-			text-align: center;
-		}
+            * {
+                box-sizing: border-box;
+            }
 
-		.certificate {
-			border: 6px double #000;
-			padding: 40px 60px;
-			width: 1000px;
-			margin: 0 auto;
-		}
+            html,
+            body {
+                margin: 0;
+                padding: 0;
+                height: 100%;
+            }
 
-		.logo {
-			float: left;
-			text-align: left;
-		}
+            body {
+                font-family: "Book Antiqua", serif;
+                position: relative;
+            }
 
-		.logo img {
-			width: 100px;
-		}
+            /* Background full-page */
+            .bg-image {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                z-index: 0;
+            }
 
-		.title {
-			font-size: 36px;
-			font-weight: bold;
-			text-decoration: underline;
-			margin-bottom: 20px;
-		}
+            /* Area teks di sisi kanan (hindari strip kiri) */
+            .content {
+                position: relative;
+                z-index: 1;
+                padding-top: 40mm;
+                padding-left: 46mm;
+                padding-right: 25mm;
+                padding-bottom: 25mm;
+                display: flex;
+                flex-direction: column;
+                color: #000;
+            }
 
-		.subtitle {
-			font-size: 20px;
-			margin: 10px 0;
-		}
+            .text-block {
+                font-size: 14pt;
+                line-height: 1.5;
+            }
 
-		.participant-name {
-			font-size: 26px;
-			font-weight: bold;
-			margin: 20px 0;
-		}
+            .label {
+                margin: 0 0 2mm 0;
+            }
 
-		.diklat-title {
-			font-size: 22px;
-			font-weight: bold;
-			text-transform: uppercase;
-			margin: 30px 0;
-		}
+            .participant-name {
+                font-size: 22pt;
+                font-weight: 700;
+                text-transform: uppercase;
+                margin: 2mm 0 8mm 0;
+            }
 
-		.date {
-			margin-top: 40px;
-			font-size: 16px;
-		}
+            .role {
+                font-size: 20pt;
+                font-weight: 700;
+            }
 
-		.signatures {
-			display: flex;
-			justify-content: space-between;
-			margin-top: 60px;
-			font-size: 16px;
-		}
+            .section-space {
+                margin-top: 6mm;
+            }
 
-		.signatures div {
-			text-align: center;
-		}
+            .diklat-title {
+                font-size: 20pt;
+                font-weight: 700;
+                text-align: center;
+                margin: 14mm 0 6mm 0;
+            }
 
-		.signature-name {
-			margin-top: 80px;
-			font-weight: bold;
-		}
+            .place-date {
+                font-size: 11pt;
+                text-align: center;
+                margin-top: 2mm;
+            }
+        </style>
+    </head>
 
-		.signature-title {
-			margin-top: 5px;
-			font-size: 14px;
-		}
-	</style>
-</head>
+    <body>
+        <!-- Background image: pakai public_path agar DomPDF bisa baca -->
+        <img
+            src="{{ public_path('mails/images/bg-diklat-certificate.jpg') }}"
+            class="bg-image"
+        />
 
-<body>
-	<div class="certificate">
+        <!-- Konten sertifikat -->
+        <div class="content">
+            <div class="text-block">
+                <p class="label">Diberikan Kepada :</p>
+                <p class="participant-name">{{ strtoupper($user->nama) }}</p>
 
-		<div class="title">Sertifikat</div>
+                <p class="label">
+                    Atas Partisipasinya Sebagai :
+                    <span class="role">PESERTA</span>
+                </p>
 
-		<div class="subtitle">Diberikan Kepada :</div>
-		<div class="participant-name">{{ $user->nama }}</div>
-		<div class="subtitle">Atas Partisipasinya Sebagai :</div>
-		<div class="subtitle" style="font-weight: bold;">PESERTA</div>
+                <p class="label section-space">Dalam Kegiatan :</p>
+                <p class="diklat-title">{{ $diklat->nama }}</p>
 
-		<div class="diklat-title">{{ strtoupper($diklat->nama) }}</div>
-
-		<div class="date">RS Kasih Ibu Surakarta, {{ \Carbon\Carbon::parse($diklat->tgl_mulai)->locale('id')->isoFormat('D MMMM YYYY') }}</div>
-
-		<div class="signatures">
-			<div>
-				<div class="signature-name">Dr. Ndarumuti Pangesti, SpPD-KEMD</div>
-				<div class="signature-title">Direktur RS Kasih Ibu</div>
-			</div>
-			<div>
-				<div class="signature-name">Dr. Ranissa Eka Sukmaningtyas</div>
-				<div class="signature-title">Diklat RS Kasih Ibu</div>
-			</div>
-		</div>
-	</div>
-</body>
-
+                <p class="place-date">
+                    RS Kasih Ibu Surakarta, {{
+                    \Carbon\Carbon::parse($diklat->tgl_mulai)->locale('id')->isoFormat('D
+                    MMMM YYYY') }}
+                </p>
+            </div>
+        </div>
+    </body>
 </html>
